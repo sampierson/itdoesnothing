cf0b4293d7a3febe7c57e8b0b3ba722f076b9990
Sam Pierson <sam@sampierson.com>
2011-12-27 16:40:56 -0800

    rails new itdoesnothing --skip-test-unit --database=sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itdoesnothing::Application.config.secret_token = 'd91799549e43415fe08785d30e080be75cdda1898f1c2cef997e37de01d7643a45c211e84df245890c0a0ec9fca9ee66c43216cd8341f97d525651d324a701ac'
