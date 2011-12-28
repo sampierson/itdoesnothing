b4c00e4434f0f32ae14285950356c757acb4aa1a
Sam Pierson <sam@sampierson.com>
2011-12-27 17:56:39 -0800

    [presentation] HAMLize and beef up application layout
    Added header/flash/footer
    Added css classes on <body>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def site_title_helper
+    "ItDoesNothing" + (Rails.env == 'production' ? "" : (" " + Rails.env))
+  end
+
+  def body_css_classes
+    [
+      underscored_controller_name,
+      underscored_controller_name + "_" + action_name
+    ].join(" ")
+  end
+
+  def underscored_controller_name
+    controller.class.name.underscore.gsub('/', '_')
+  end
 end
